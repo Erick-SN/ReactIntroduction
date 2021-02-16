47cc182a19ab3fb606365b77493aa4fdda4fc918
Erick-SN <ericksn0707@gmail.com>
2021-02-15 21:02:56 -0600

    Not found page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Create from './components/Create';
 import BlogDetails from './components/BlogDetails';
+import NotFound from './components/NotFound';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
               <Route exact path="/" component={Home} />
               <Route exact path="/create" component={Create} />
               <Route exact path="/blogs/:id" component={BlogDetails} />
+              <Route path="*" component={NotFound} />
             </Switch>
           </div>
         </div>
